d42923e22d664fd7413b2edab17cf3472956ac8a
18A91A0479 <86997930+18A91A0479@users.noreply.github.com>
2022-03-31 09:23:16 +0530

    Delete LoginService.java

--- LoginService.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-public interface LoginService
-{
-    boolean loginAsApplicant(String username,String password );
-    boolean loginAsAdmissionCommiteeMember(String username,String password);
-    boolean loginAsUniversityStaffMember(String username,String password);
-}
